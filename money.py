d99877fb4436085caf246cb600c734f2cfee1f7f
Calixd7 <calixd7@gmail.com>
2020-12-15 09:31:07 -0500

    only 5 I got to passed

--- money.py
+++ money.py
@@ -18,13 +18,17 @@ class Currency:
         - symbol - optional symbol used to designate currency
         - digits -- number of significant digits used
         """
-        pass
+        self.name =name
+        self.code =code
+        self.symbol = symbol
+        self.digits =digits
+       
 
     def __str__(self):
         """
         Should return the currency code, or code with symbol in parentheses.
         """
-        pass
+        return f"{self.code}{self.symbol}"
 
     def __eq__(self, other):
         """
@@ -46,48 +50,47 @@ class Money:
         - amount -- quantity of currency
         - currency -- type of currency
         """
-        pass
+        self.amount =  amount
+        self.currency = currency
 
     def __str__(self):
+       
         """
         Should use the currency symbol if available, else use the code.
         Use the currency digits to determine number of digits to show.
         """
-        pass
 
     def __repr__(self):
-        return f"<Money {str(self)}>"
 
-    def __eq__(self, other):
+     def __eq__(self, other):
         """
         All fields must be equal to for the objects to be equal.
         """
 
-        return (type(self) == type(other) and self.amount == other.amount and
-                self.currency == other.currency)
 
     def add(self, other):
+
         """
         Add two money objects of the same currency. If they have different
         currencies, raise a DifferentCurrencyError.
         """
-        pass
 
     def sub(self, other):
+    
         """
         Subtract two money objects of the same currency. If they have different
         currencies, raise a DifferentCurrencyError.
         """
-        pass
 
     def mul(self, multiplier):
         """
         Multiply a money object by a number to get a new money object.
         """
-        pass
+       
 
     def div(self, divisor):
         """
         Divide a money object by a number to get a new money object.
         """
-        pass
+     
+
